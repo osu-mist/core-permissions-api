c2d8a0e2ff955b6c8af733876e445677456313f6
ianbrown9475 <32145829+ianbrown9475@users.noreply.github.com>
2018-08-06 11:43:21 -0700

    Remove docker.sh

--- demo/docker.sh
+++
@@ -1,7 +0,0 @@
-docker build -t core-permissions-demo .
-
-docker run -d -it --rm \
-    -p 5000:5000 \
-    -v "$PWD"/configuration.py:/usr/src/app/configuration.py:ro \
-    --name core-permissions-demo \
-    core-permissions-demo
