52f0efd1ca314c2f9a50e7d07ded9c1992565835
Jared Kosanovic <kosanovj@oregonstate.edu>
2018-08-17 10:55:40 -0500

    use initialize from super class

--- src/main/groovy/edu/oregonstate/mist/permissionsapi/PermissionsApplication.groovy
+++ src/main/groovy/edu/oregonstate/mist/permissionsapi/PermissionsApplication.groovy
@@ -3,7 +3,6 @@ package edu.oregonstate.mist.permissionsapi
 import edu.oregonstate.mist.api.Application
 import edu.oregonstate.mist.permissionsapi.db.PermissionsDAO
 import edu.oregonstate.mist.permissionsapi.resources.PermissionsResource
-import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
 import io.dropwizard.jdbi.DBIFactory
 import org.skife.jdbi.v2.DBI
@@ -18,18 +17,16 @@ class PermissionsApplication extends Application<PermissionsConfiguration> {
      * @param configuration
      * @param environment
      */
-    @Override
-    void initialize(Bootstrap<PermissionsConfiguration> bootstrap) {}
 
     @Override
     void run(PermissionsConfiguration configuration, Environment environment) {
+        this.setup(configuration, environment)
+
         final DBIFactory FACTORY = new DBIFactory()
         final DBI JDBI = FACTORY.build(environment, configuration.getDataSourceFactory(), "jdbi")
         final PermissionsDAO DAO = JDBI.onDemand(PermissionsDAO.class)
 
         environment.jersey().register(new PermissionsResource(DAO, configuration.api.endpointUri))
-
-        this.setup(configuration, environment)
     }
 
     /**
